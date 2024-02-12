c5cc9380fa84f4221c34854d6150cdff76af9f43
Subaru Goto <subaru.dayo@gmail.com>
2024-02-12 11:13:48 +0100

    adding cors

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import express from "express";
 import { connectDatabase } from "./db/client.js";
 import { apiRouter } from "./routes/api.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 8001;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api/countries", apiRouter);
