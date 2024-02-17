05d65bb4c39c3ec92fe91da97cdd868e085f72b3
Subaru Goto <subaru.dayo@gmail.com>
2024-02-17 18:29:57 +0100

    change name

--- controllers/students.js
+++ controllers/students.js
@@ -32,14 +32,14 @@ export const registerStudent = tryAndCatch(
 export const logInStudent = tryAndCatch(
   async (req, res, next) => {
     const { email, password } = req.body;
-    const data = await Student.findOne({email: email});
+    const student = await Student.findOne({email: email});
     if(!data) {return res.status(404).send("! User not found");}
 
     const validPassword = await bcrypt.compare(password, data.password);
     if(! validPassword) {return res.status(400).send("! Invalid Credential");}
     
     const token = generateToken({email: data.email, id: data._id });
-    res.json({ token, data });
+    res.json({ token, student });
   }
 )
 
